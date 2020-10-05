277729b7d60aba58d675eb5256125cb9bf554f23
TejasBhavsar03 <72385786+TejasBhavsar03@users.noreply.github.com>
2020-10-05 16:10:36 +0530

    Learned basics and numeric data types

---
+++ pythonudemy.py
@@ -0,0 +1,30 @@
+a=13
+b=6
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a**b)           #Exponent
+print(a/b)            #Division, this will return floating value
+print(a//b)           #Double Division
+print(a%b)
+print(a+(b-a)+b)
+
+#*********************************
+
+c = 11
+
+# c = c+1
+# c += 1
+# c = c-2
+# c -= 2
+# c = c*2
+# c *= 2
+# c = c/2
+# c //= 2
+# c = c**2
+# c **= 2
+# c = c%2
+c %= 2
+
+print(c)
