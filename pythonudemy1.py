fbabc35f74f2048b79947e3e7241b029e3357bee
TejasBhavsar03 <72385786+TejasBhavsar03@users.noreply.github.com>
2020-10-06 20:45:15 +0530

    Learned String Data Type

---
+++ pythonudemy1.py
@@ -0,0 +1,84 @@
+# String:Sequence of character
+
+# string = 'sequence ofhe said "this is not goog" characters'
+# print(string)
+# Slice
+
+
+# name = "Gangster"
+# Indexing
+# print(name)
+# print(name[0::])
+# print(name[0:7:3])
+# # print(name[1])
+# # print(name[2])
+# # print(name[3])
+# # print(name[4])
+# # print(name[5])
+# # print(name[6])
+# # print(name[7])
+# # "Gangster"
+# print(name[-8::])
+# # print(name[-8])
+# # print(name[-7])
+# # print(name[-6])
+# # print(name[-5])
+# # print(name[-4])
+# # print(name[-3])
+# # print(name[-2])
+# # print(name[-1])
+# # # print(name)
+#
+# string1 =' 1 2 3 4 5 6 7 8 9'
+# print(string1[0::2])
+
+
+# name = "Gangster"
+#
+# job = " play cricket"
+#
+# # a = 1
+# # b = 1
+# # print(a+b)
+# # c= '1'
+# # d= '1'
+# # print(c+d)
+# print(name*10)
+
+# string1 = 'This'
+# string2 = 'That'
+# print(string1+'sdfsdfsdf'+string2)
+#
+# print('x'*50)
+#
+# print(string1+'sdfsdfsdf'+string2)
+# message = 'Hello Tejas welcome,you are 20 years old'
+# print(message)
+
+# message2 = 'Hello {2} welcome,you are {0} years old, you r {1}'.format(name,age,gender)
+# print(message1)
+
+
+#
+# name = input('Please enter your name: ')
+# age = int(input('Please enter your age:'))
+# # age = 10
+# message1 = f'Hello {name} welcome,you\'s are {age/2} years old'
+# message2 = f'Hello {name} welcome,you\ts are {age/2} years old'
+# message3 = f'Hello {name} welcome,you\ns are {age/2} years old'
+# print(message1)
+# print(message2)
+# print(message3)
+
+
+# Strings Methods
+
+name = "GANGSTER is HOME and I am happy"
+print(name.lower())
+print(name.upper())
+print(name.strip())
+print(name.split())
+print(name.find('is'))
+print(name.replace('is', 'was'))
+print(name.startswith('G'))
+print(name.endswith('sa'))
