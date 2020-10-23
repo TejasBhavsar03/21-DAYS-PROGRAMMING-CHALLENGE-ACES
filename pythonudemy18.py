886ce8566d718098e30e81c9bacdeab94ed0fb58
TejasBhavsar03 <72385786+TejasBhavsar03@users.noreply.github.com>
2020-10-23 20:35:55 +0530

    Learned Decorators

---
+++ pythonudemy18.py
@@ -0,0 +1,15 @@
+def chees_and_buns(orogonal_fun):
+    def wrap():
+        print('I am upper bread')
+        orogonal_fun()
+        print('I am lower break')
+
+    return wrap()
+
+
+def chicken():
+    print('I am roasted chicken')
+
+
+chicken()
+# burger = chees_and_buns(chicken)
